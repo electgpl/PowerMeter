1304a30614fd3a321c345e1b0f91c8390ef48aea
Sebastian Caccavallo <electgpl@gmail.com>
2018-08-11 16:48:20 -0300

    Delete Power_Meter_v1.0.bat

--- Power_Meter_v1.0.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/eagle3d/ulp//../povray" +O"C:/Users/electgpl/Desktop/Nueva carpeta (3)/Power_Meter_v1.0.png" /RENDER "C:/Users/electgpl/Desktop/Nueva carpeta (3)/Power_Meter_v1.0.pov" 
